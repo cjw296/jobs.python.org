06af971758d5e723ef12cc4d4f7856ff0f99316c
Chris Withers <chris@simplistix.co.uk>
2011-05-24 20:42:26 +0100

    configure session and move username/password out of source control

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,4 @@
-from mortar_rdb import registerSession, getSession
+from mortar_rdb import registerSession
 from pyramid.config import Configurator
 
 import logging
@@ -10,6 +10,9 @@ def main(global_config, **settings):
     This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    with open(settings['sqlalchemy.url_file']) as db_url:
+        registerSession(db_url.read().strip())
     
     config.scan()
     
