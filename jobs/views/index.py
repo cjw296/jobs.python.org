efb29cf06f882e0a8d0cee0762aad4370f8fd511
Chris Withers <chris@simplistix.co.uk>
2011-05-30 22:31:26 +0100

    some more dummy templates

--- jobs/views/index.py
+++ jobs/views/index.py
@@ -5,3 +5,10 @@ def index(context,request):
     return dict(
         )
 
+@view_config(name='secure_index',
+             renderer='templates/secure_index.pt',
+             permission='authenticated')
+def secure_index(context,request):
+    return dict(
+        )
+
--- jobs/views/macros.py
+++ jobs/views/macros.py
@@ -3,7 +3,12 @@ from pyramid.events import (
     subscriber ,
     )
 from pyramid.renderers import get_renderer
+from pyramid.security import authenticated_userid
 
 @subscriber(BeforeRender)
 def add_site_template(event):
+    request = event['request']
+    
     event['site_template'] = get_renderer('templates/site.pt').implementation()
+
+    event['user_id'] = authenticated_userid(request)
